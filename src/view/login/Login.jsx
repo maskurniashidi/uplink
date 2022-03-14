3c62349e628de63041f4d0ee5ead3b027454244d
Bimzking1 <kingofbimo@gmail.com>
2022-03-14 17:02:07 +0700

    Sign Up - Register Fixes #12

--- src/view/login/Login.jsx
+++ src/view/login/Login.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import style from "./ContactUs.module.css";
+// import React from "react";
+// import style from "./ContactUs.module.css";
 
-//component
-import NavHome from "../../components/nav/NavHome";
-import Footer from "../../components/footer/Footer";
+// //component
+// import NavHome from "../../components/nav/NavHome";
+// import Footer from "../../components/footer/Footer";
 
-function ContactUs() {
-  return (
-    <div className={style.contactus}>
-      <div className={style.navbarContainer}>
-        <NavHome/>
-      </div>
-      <div className={style.contentContainer}>
-        Isinya mas ryan
-      </div>
-      <div className={style.footerContainer}>
-        <Footer/>
-      </div>
-    </div>
-  );
-}
+// function ContactUs() {
+//   return (
+//     <div className={style.contactus}>
+//       <div className={style.navbarContainer}>
+//         <NavHome/>
+//       </div>
+//       <div className={style.contentContainer}>
+//         Isinya mas ryan
+//       </div>
+//       <div className={style.footerContainer}>
+//         <Footer/>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default ContactUs;
+// export default ContactUs;
